250cbe358acba5f95ebdab8d9ed602f3e034d223
prasanths1991 <123563297+prasanths1991@users.noreply.github.com>
2024-01-23 14:14:48 +0530

    Update execute_programs.sh

--- execute_programs.sh
+++ execute_programs.sh
@@ -2,7 +2,7 @@
 pip install -r requirements.txt
 python -m virtualenv afour
 .\afour\Scripts\activate
-python pattern.py
-python flight_status.py
+python PATTERN\pattern.py
+python FLIGHT\flight_status.py
 sleep 10
 deactivate
